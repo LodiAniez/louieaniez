85983e4580930746e269af6ab7cec881a734b8b6
aniezlouie <louie@findersforce.com>
2024-07-24 11:15:51 +0800

    WIP

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-let assetPrefix = ''
-let basePath = '/'
+let assetPrefix = "";
+let basePath = process.env.BASE_PATH || "";
 
 if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
 }
 
 module.exports = {
   assetPrefix: assetPrefix,
   basePath: basePath,
-  output: 'export',
-}
+  output: "export",
+};
---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+
+const StyledBox = styled(Box)(() => ({
+  display: "flex",
+}));
+
+const Wrapper = () => {
+  return <StyledBox>Work in Progress.</StyledBox>;
+};
+
+export default Wrapper;
