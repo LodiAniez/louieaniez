eaa849bfd2ba02c4072d647016341f636a4625bf
aniezlouie <louie@findersforce.com>
2024-07-26 17:56:17 +0800

    Added tags for each project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,79 +10,14 @@ import AppIcon from "@mui/icons-material/AppsOutlined";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
-import { basePath } from "./../../next.config";
 
 import Carousel from "@/components/Carousel";
 import PageLayout from "@/layouts/page-layout";
 
-enum ProjectTag {
-  WEB = "Web application",
-  MOBILE = "Mobile application",
-}
-
-type Project = {
-  title: string;
-  tags: ProjectTag[];
-  images: string[];
-  description: string;
-  link?: string;
-};
+import { useProjects } from "@/hooks/useProjects";
 
 const Project = () => {
-  const { MOBILE, WEB } = ProjectTag;
-  const projects: Project[] = [
-    {
-      title: "Canvaschat",
-      tags: [WEB],
-      images: [
-        `${basePath}/canvaschat/canvaschattool.png`,
-        `${basePath}/canvaschat/jackpinemedia.png`,
-      ],
-      description:
-        "Canvaschat is a versatile chat portal that can be embedded into any business website to address customer concerns. It includes an agent portal for responding to customer queries and an admin portal for viewing reports and feedback.",
-      link: "https://canvaschattool.com/",
-    },
-    {
-      title: "Meetsone",
-      tags: [WEB, MOBILE],
-      images: [
-        `${basePath}/meetsone/1.png`,
-        `${basePath}/meetsone/meetsone1.png`,
-        `${basePath}/meetsone/meetsone2.png`,
-        `${basePath}/meetsone/meetsone3.png`,
-      ],
-      description:
-        "Meetsone is a Japanese app that provides businesses with flexible control over their processes and transactions through dynamic forms and efficient reporting, offering functionality similar to a CRM system.",
-      link: "https://app.meetsone.com/mm-login",
-    },
-    {
-      title: "Fancrew",
-      tags: [WEB],
-      images: [
-        `${basePath}/fancrew/fancrew1.png`,
-        `${basePath}/fancrew/fancrew2.png`,
-      ],
-      description:
-        "Fancrew is a web app that enables businesses to create dynamic questionnaires and surveys, helping them gather valuable insights from their customers.",
-      link: "https://dev.cr.fancrew.com/admin/login",
-    },
-    {
-      title: "Requisition app",
-      tags: [WEB],
-      images: [
-        `${basePath}/mymc/mymc1.jpg`,
-        `${basePath}/mymc/mymc2.jpg`,
-        `${basePath}/mymc/mymc3.jpg`,
-        `${basePath}/mymc/mymc4.jpg`,
-      ],
-      description:
-        "Requisition app centralizes the manual process of sending equipment requests from various business addresses to the main office and is deployed internally. It also includes a feature for maintaining an inventory record of each piece of equipment.",
-    },
-  ];
-
-  const handleOpenLink = (link: string) => {
-    window.open(link, "_blank");
-  };
+  const { handleOpenLink, projects } = useProjects();
 
   return (
     <PageLayout>
@@ -100,54 +35,71 @@ const Project = () => {
 
       <Box className="flex flex-col gap-2">
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          {projects.map(({ description, images, tags, title, link }, index) => (
-            <Grid item xs={6} key={index}>
-              <Card sx={{ backgroundColor: "#16212b" }}>
-                <CardHeader
-                  avatar={
-                    <Avatar aria-label="appicon">
-                      <AppIcon className="text-[#16212b]" />
-                    </Avatar>
-                  }
-                  title={title}
-                  subheader={tags.join(" | ")}
-                  titleTypographyProps={{
-                    fontWeight: 600,
-                    color: "#fff",
-                  }}
-                  subheaderTypographyProps={{
-                    color: "#ccc",
-                  }}
-                />
-                <CardMedia component="image">
-                  <Carousel images={images} />
-                </CardMedia>
-                <CardContent
-                  className="flex flex-col gap-2"
-                  sx={{
-                    color: "#e1e1e3",
-                    height: 170,
-                    overflow: "auto",
-                  }}
-                >
-                  <Typography variant="body2">
-                    Link:{" "}
-                    {link ? (
-                      <Link
-                        onClick={() => handleOpenLink(link)}
-                        className="cursor-pointer"
-                      >
-                        {link}
-                      </Link>
-                    ) : (
-                      <em>N / A (Internal)</em>
-                    )}
-                  </Typography>
-                  <Typography variant="body2">{description}</Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
+          {projects.map(
+            (
+              { description, images, tags, title, link, technologies },
+              index
+            ) => (
+              <Grid item xs={6} key={index}>
+                <Card sx={{ backgroundColor: "#16212b" }}>
+                  <CardHeader
+                    avatar={
+                      <Avatar aria-label="appicon">
+                        <AppIcon className="text-[#16212b]" />
+                      </Avatar>
+                    }
+                    title={title}
+                    subheader={tags.join(" | ")}
+                    titleTypographyProps={{
+                      fontWeight: 600,
+                      color: "#fff",
+                    }}
+                    subheaderTypographyProps={{
+                      color: "#ccc",
+                    }}
+                  />
+                  <CardMedia component="image">
+                    <Carousel images={images} />
+                  </CardMedia>
+                  <CardContent
+                    className="flex flex-col gap-2"
+                    sx={{
+                      color: "#e1e1e3",
+                      height: 170,
+                      overflow: "auto",
+                    }}
+                  >
+                    <Typography variant="body2">
+                      Link:{" "}
+                      {link ? (
+                        <Link
+                          onClick={() => handleOpenLink(link)}
+                          className="cursor-pointer"
+                        >
+                          {link}
+                        </Link>
+                      ) : (
+                        <em>N / A (Internal)</em>
+                      )}
+                    </Typography>
+                    <Typography variant="body2">{description}</Typography>
+                    <Grid container spacing={0.5}>
+                      {technologies.map((tech, techIndex) => (
+                        <Grid item xs={4} key={techIndex}>
+                          <Typography
+                            sx={{ bgcolor: tech.color, color: "#fff" }}
+                            className="p-1 rounded-md text-center text-xs"
+                          >
+                            {tech.tech}
+                          </Typography>
+                        </Grid>
+                      ))}
+                    </Grid>
+                  </CardContent>
+                </Card>
+              </Grid>
+            )
+          )}
         </Grid>
       </Box>
     </PageLayout>
---
+++ src/hooks/useProjects.ts
@@ -0,0 +1,260 @@
+"use client";
+
+import { basePath } from "./../../next.config";
+
+enum Technologies {
+  NODEJS = "NodeJS",
+  TS = "Typescript",
+  JS = "Javascript",
+  TAILWIND = "Tailwind CSS",
+  MUI = "Material UI",
+  MYSQL = "MySQL",
+  NOSQL = "NoSQL",
+  DOCKER = "Docker",
+  GIT = "Git",
+  POSTGRE = "PostgreSQL",
+  FIREBASE = "Firebase",
+  MONGO = "MongoDB",
+  PRISMA = "Prisma",
+  NEXT = "Next JS",
+  NEST = "Nest JS",
+  RTK = "Redux Toolkit Query",
+  LINUX = "Linux",
+  SOCKETIO = "Socket.IO",
+  LARAVEL = "Laravel",
+  ANGULAR = "Angular",
+  NGRX = "NgRx",
+  REACT = "React JS",
+  RN = "React Native",
+}
+
+enum ProjectTag {
+  WEB = "Web application",
+  MOBILE = "Mobile application",
+}
+
+type Techs = Record<Technologies, { color: string; tech: Technologies }>;
+type Project = {
+  title: string;
+  tags: ProjectTag[];
+  images: string[];
+  description: string;
+  technologies: { color: string; tech: Technologies }[];
+  link?: string;
+};
+
+export const useProjects = () => {
+  const { MOBILE, WEB } = ProjectTag;
+  const {
+    DOCKER,
+    FIREBASE,
+    GIT,
+    JS,
+    LINUX,
+    MONGO,
+    MUI,
+    MYSQL,
+    NEST,
+    NEXT,
+    NODEJS,
+    NOSQL,
+    POSTGRE,
+    PRISMA,
+    RTK,
+    SOCKETIO,
+    TAILWIND,
+    TS,
+    LARAVEL,
+    ANGULAR,
+    NGRX,
+    REACT,
+    RN,
+  } = Technologies;
+
+  const techs: Techs = {
+    [DOCKER]: {
+      color: "#242323",
+      tech: DOCKER,
+    },
+    [FIREBASE]: {
+      color: "#f0331a",
+      tech: FIREBASE,
+    },
+    [GIT]: {
+      color: "#1b9905",
+      tech: GIT,
+    },
+    [JS]: {
+      color: "#1b3d15",
+      tech: JS,
+    },
+    [LINUX]: {
+      color: "#101a0e",
+      tech: LINUX,
+    },
+    [MONGO]: {
+      color: "#3d61b8",
+      tech: MONGO,
+    },
+    [MUI]: {
+      color: "#071c4f",
+      tech: MUI,
+    },
+    [MYSQL]: {
+      color: "#021d1f",
+      tech: MYSQL,
+    },
+    [NEST]: {
+      color: "#8f0778",
+      tech: NEST,
+    },
+    [NEXT]: {
+      color: "#594c57",
+      tech: NEXT,
+    },
+    [NODEJS]: {
+      color: "#80203b",
+      tech: NODEJS,
+    },
+    [NOSQL]: {
+      color: "#343630",
+      tech: NOSQL,
+    },
+    [POSTGRE]: {
+      color: "#101f0f",
+      tech: POSTGRE,
+    },
+    [PRISMA]: {
+      color: "#131413",
+      tech: PRISMA,
+    },
+    [RTK]: {
+      color: "#40222b",
+      tech: RTK,
+    },
+    [SOCKETIO]: {
+      color: "#87334c",
+      tech: SOCKETIO,
+    },
+    [TAILWIND]: {
+      color: "#151e36",
+      tech: TAILWIND,
+    },
+    [TS]: {
+      color: "#10121a",
+      tech: TS,
+    },
+    [LARAVEL]: {
+      color: "#402566",
+      tech: LARAVEL,
+    },
+    [ANGULAR]: {
+      color: "#023308",
+      tech: ANGULAR,
+    },
+    [NGRX]: {
+      color: "#020733",
+      tech: NGRX,
+    },
+    [REACT]: {
+      color: "#023d42",
+      tech: REACT,
+    },
+    [RN]: {
+      color: "#1a0101",
+      tech: RN,
+    },
+  };
+
+  const projects: Project[] = [
+    {
+      title: "Canvaschat",
+      tags: [WEB],
+      images: [
+        `${basePath}/canvaschat/canvaschattool.png`,
+        `${basePath}/canvaschat/jackpinemedia.png`,
+      ],
+      description:
+        "Canvaschat is a versatile chat portal that can be embedded into any business website to address customer concerns. It includes an agent portal for responding to customer queries and an admin portal for viewing reports and feedback.",
+      link: "https://canvaschattool.com/",
+      technologies: [
+        techs[NODEJS],
+        techs[ANGULAR],
+        techs[TS],
+        techs[JS],
+        techs[SOCKETIO],
+        techs[MYSQL],
+        techs[GIT],
+        techs[LINUX],
+        techs[NGRX],
+      ],
+    },
+    {
+      title: "Meetsone",
+      tags: [WEB, MOBILE],
+      images: [
+        `${basePath}/meetsone/1.png`,
+        `${basePath}/meetsone/meetsone1.png`,
+        `${basePath}/meetsone/meetsone2.png`,
+        `${basePath}/meetsone/meetsone3.png`,
+      ],
+      description:
+        "Meetsone is a Japanese app that provides businesses with flexible control over their processes and transactions through dynamic forms and efficient reporting, offering functionality similar to a CRM system.",
+      link: "https://app.meetsone.com/mm-login",
+      technologies: [
+        techs[NEST],
+        techs[NEXT],
+        techs[DOCKER],
+        techs[PRISMA],
+        techs[RTK],
+        techs[GIT],
+        techs[POSTGRE],
+        techs[TS],
+        techs[JS],
+        techs[LINUX],
+      ],
+    },
+    {
+      title: "Fancrew",
+      tags: [WEB],
+      images: [
+        `${basePath}/fancrew/fancrew1.png`,
+        `${basePath}/fancrew/fancrew2.png`,
+      ],
+      description:
+        "Fancrew is a web app that enables businesses to create dynamic questionnaires and surveys, helping them gather valuable insights from their customers.",
+      link: "https://dev.cr.fancrew.com/admin/login",
+      technologies: [
+        techs[REACT],
+        techs[RTK],
+        techs[GIT],
+        techs[LINUX],
+        techs[DOCKER],
+        techs[NEST],
+        techs[POSTGRE],
+      ],
+    },
+    {
+      title: "Requisition app",
+      tags: [WEB],
+      images: [
+        `${basePath}/mymc/mymc1.jpg`,
+        `${basePath}/mymc/mymc2.jpg`,
+        `${basePath}/mymc/mymc3.jpg`,
+        `${basePath}/mymc/mymc4.jpg`,
+      ],
+      description:
+        "Requisition app centralizes the manual process of sending equipment requests from various business addresses to the main office and is deployed internally. It also includes a feature for maintaining an inventory record of each piece of equipment.",
+      technologies: [techs[ANGULAR], techs[NGRX], techs[GIT], techs[MYSQL]],
+    },
+  ];
+
+  const handleOpenLink = (link: string) => {
+    window.open(link, "_blank");
+  };
+
+  return {
+    handleOpenLink,
+    projects,
+  };
+};
